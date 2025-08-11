c9e9170eafdbd237b367d286d58118f173e0c0cd
Vaibhav Singh <singhvaibhav654@gmail.com>
2025-08-11 15:50:09 +0530

    correct nv

--- src/app/component/nav.js
+++ src/app/component/nav.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="bg-white border-b border-gray-200">
-      <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex flex-wrap p-4 items-center justify-center md:justify-between">
         
         {/* Logo + Name */}
         <Link
@@ -24,43 +24,36 @@ export default function Navbar() {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-2 text-lg font-semibold text-indigo-600">Humayl Fazal</span>
+          <span className="ml-2 text-lg font-semibold text-indigo-600">
+            Humayl Fazal
+          </span>
         </Link>
 
-
-        <nav className="md:ml-auto flex flex-wrap items-center text-sm md:text-base text-indigo-700">
+        {/* Navigation Links */}
+        <nav className="w-full md:w-auto flex flex-wrap justify-center md:justify-end text-sm md:text-base text-indigo-700">
           <Link href="/" className="mr-5 hover:text-black transition">
             Home
           </Link>
-       
           <Link href="/leadership" className="mr-5 hover:text-black transition">
             Leadership
-          </Link> 
-
-  
-
-   <Link href="/compound_activity#sports" className="mr-5 hover:text-black transition">
+          </Link>
+          <Link href="/compound_activity#sports" className="mr-5 hover:text-black transition">
             Sports & Competitions
           </Link>
-
           <Link href="/compound_activity#projects" className="mr-5 hover:text-black transition">
             Projects
           </Link>
-
           <Link href="/compound_activity#research" className="mr-5 hover:text-black transition">
             Writing
           </Link>
-
           <Link href="/compound_activity#workex" className="mr-5 hover:text-black transition">
             Work Experience
           </Link>
-       
-            <Link href="/awards" className="mr-5 hover:text-black transition">
+          <Link href="/awards" className="mr-5 hover:text-black transition">
             Awards
           </Link>
         </nav>
 
-   
       </div>
     </header>
   );
